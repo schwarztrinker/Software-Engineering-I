3938dbafb3eb918977aa62588f578d369667029a
schwarztrinker <38423981+schwarztrinker@users.noreply.github.com>
2018-12-11 18:49:13 +0100

    Update ReadingDevice.java

--- src/Airport/Bulky_Baggage_Desk/ReadingDevice.java
+++ src/Airport/Bulky_Baggage_Desk/ReadingDevice.java
@@ -1,9 +1,21 @@
 package Airport.Bulky_Baggage_Desk;
 
+import Airport.Base.BoardingPass;
+import Airport.Base.Passport;
+
 public class ReadingDevice {
 
     private String uuid;
     private String type;
     private String id;
+
+
+
+    String version(){ return "Version";}
+
+    boolean scan(Passport passport){return false; }
+
+    boolean scan(BoardingPass boardingPass) {return false; }
+
 
 }
