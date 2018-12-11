7c410fd690ea88d6a9ead21132e3bd34aa9f3dbf
schwarztrinker <38423981+schwarztrinker@users.noreply.github.com>
2018-12-11 17:52:52 +0100

    baggagedesk

--- src/Airport/Bulky_Baggage_Desk/BulkyBaggageDesk.java
+++ src/Airport/Bulky_Baggage_Desk/BulkyBaggageDesk.java
@@ -1,4 +1,16 @@
 package Airport.Bulky_Baggage_Desk;
 
 public class BulkyBaggageDesk {
+
+    private String uuid;
+    private SecurityCheckID securityCheckID;
+    private Airport airport;
+    private List<Employee> employeeList;
+    private FederalPoliceOfficer policeOfficer;
+    private List<String> scanPatternList;
+    private IBaggageScanner baggageScanner;
+    private IBodyScanner bodyScanner;
+    private IExplosivesDetector explosivesDetector;
+    private FederalPolice federalPolice;
+
 }
