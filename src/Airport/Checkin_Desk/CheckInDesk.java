d84bc6ca93a4d6941ab08f21410f3398c54ac1ee
schwarztrinker <38423981+schwarztrinker@users.noreply.github.com>
2018-12-22 12:38:50 +0100

    final

--- src/Airport/Checkin_Desk/CheckInDesk.java
+++ src/Airport/Checkin_Desk/CheckInDesk.java
@@ -1,11 +1,14 @@
 package Airport.Checkin_Desk;
 
+import Airport.Airport.Airport;
 import Airport.Airport.GateID;
 import Airport.Base.*;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
 
-public class CheckInDesk {
+public class CheckInDesk implements ICheckInDesk{
 
     private String uuid;
     private int id;
@@ -25,10 +28,87 @@ public class CheckInDesk {
     private ArrayList<BoardingPass> boardingPassList;
     private ArrayList<BaggageIdentificationTag> baggageIdentificationTagList;
     private boolean isOpen;
+    private boolean notifiedGroundOperation;
 
 
+    public CheckInDesk(Airport airport){
+        UUID uuid = UUID.randomUUID();
+        this.uuid = uuid.toString();
+        Random random = new Random();
+        id = random.nextInt(200) + 1;
+        this.airport = airport;
+        currentPassenger = airport.loadPassengerBaggageData();
+    }
 
 
+    @Override
+    public void open(){
+        isOpen = true;
+    };
 
+    @Override
+    public boolean validatePassport(Passenger passenger){ return false;};
+
+    @Override
+    public BoardingPass checkIn(Passenger passenger){
+        return currentPassenger.getBoardingPass();}
+
+
+    @Override
+    public void print(BoardingPass boardingPass){
+
+    }
+
+    @Override
+    public double weight(Baggage baggage){
+        return getRandomDouble(5, 15);
+    }
+
+
+    @Override
+    public BaggageIdentificationTag checkIn(Baggage baggage){
+        return BaggageIdentificationTag;
+    }
+
+    @Override
+    public void print(BaggageIdentificationTag baggageIdentificationTag){
+
+    }
+
+    @Override
+    public void place(BaggageIdentificationTag baggageIdentificationTag, Baggage baggage){
+        currentBaggage.setBaggageIdentificationTag(baggageIdentificationTag);
+    }
+
+    @Override
+    public void place(Baggage baggage, LuggageTub luggageTub){
+        luggageTub.setBaggage(baggage);
+
+    }
+
+    @Override
+    public void sendLuggageTubToBaggageSortingUnit(LuggageTub luggageTub){
+        airport.getBaggageSortingUnit().throwOff(luggageTub, airport.getBaggageSortingUnit().getDestinationBox());
+    }
+
+    @Override
+    public void notifyGroundOperations(CheckInDeskReceipt checkInDeskReceipt){
+        this.baggageIdentificationTagList = baggageIdentificationTagList;
+        numberOfPassenger = checkInDeskReceipt.getNumberOfPassenger();
+        id = checkInDeskReceipt.getCheckInDeskId();
+
+        notifiedGroundOperation = true;
+    }
+
+
+    @Override
+    public void close(){
+        isOpen = false;
+    }
+
+    private double getRandomDouble(int min, int max){
+        if(min < max) return  (min + Math.random() * (max - min));
+        return  (max + Math.random() * (min - max));
+    }
 
 }
--- src/Airport/Checkin_Desk/ICheckInDesk.java
+++ src/Airport/Checkin_Desk/ICheckInDesk.java
@@ -1,4 +1,21 @@
 package Airport.Checkin_Desk;
 
+import Airport.Base.*;
+import Airport.Bulky_Baggage_Desk.BulkyBaggageDeskReceipt;
+
+import java.util.ArrayList;
+
 public interface ICheckInDesk {
+    void open();
+    boolean validatePassport(Passenger passenger);
+    BoardingPass checkIn(Passenger passenger);
+    void print(BoardingPass boardingPass);
+    double weight(Baggage baggage);
+    BaggageIdentificationTag checkIn(Baggage baggage);
+    void print(BaggageIdentificationTag baggageIdentificationTag);
+    void place(BaggageIdentificationTag baggageIdentificationTag, Baggage baggage);
+    void place(Baggage baggage, LuggageTub luggageTub);
+    void sendLuggageTubToBaggageSortingUnit(LuggageTub luggageTub);
+    void notifyGroundOperations(CheckInDeskReceipt checkInDeskReceipt);
+    void close();
 }
